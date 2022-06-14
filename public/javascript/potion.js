eff19b936e96f1349f5ba0f32e77954d7cd4e042
0LordSunday0 <jackson.quinn3@gmail.com>
2022-06-14 15:37:43 +1000

    implementing draggable

---
+++ public/javascript/potion.js
@@ -0,0 +1,38 @@
+var draggables = document.querySelectorAll('.draggable');
+var containers = document.querySelectorAll('.container');
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging')
+    })
+
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+    })
+})
+
+containers.forEach(container => {
+    container.addEventListener('dragover', () => {
+        const draggable = document.querySelector('.dragging')
+        container.appendChild(draggable)
+    })
+})
+
+var gameBoard = [];
+
+var boardWidth = 3;
+var boardHeight = 3;
+
+for(let i = 0; i < boardHeight; i ++){
+    gameBoard.push([boardWidth]);
+}
+
+var gameOn = false;
+while(gameOn){
+
+
+}
+
+function renderPotion(color, position){
+
+}
