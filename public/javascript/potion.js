bcc31cf8e43c01739535d8b45a05b696f6ff5e7b
0LordSunday0 <jackson.quinn3@gmail.com>
2022-06-14 17:17:54 +1000

    making the board and spawn

--- public/javascript/potion.js
+++ public/javascript/potion.js
@@ -1,20 +1,52 @@
-var draggables = document.querySelectorAll('.draggable');
+//var draggables = document.querySelectorAll('.draggable');
 var containers = document.querySelectorAll('.container');
+var blueButton = document.querySelector('.spawn.blue');
+var redButton = document.querySelector('.spawn.red');
+var controls = document.querySelector('.controls');
 
-draggables.forEach(draggable => {
-    draggable.addEventListener('dragstart', () => {
-        draggable.classList.add('dragging')
+blueButton.addEventListener('click', function() {
+    var original = document.getElementById("blue-potion");
+    original.classList.remove('template');
+    var copy = original.cloneNode(true);
+    original.classList.add('template');
+    
+    copy.addEventListener('dragstart', () => {
+        copy.classList.add('dragging')
     })
 
-    draggable.addEventListener('dragend', () => {
-        draggable.classList.remove('dragging')
+    copy.addEventListener('dragend', () => {
+        copy.classList.remove('dragging')
     })
+
+    controls.appendChild(copy);
+    
+})
+
+redButton.addEventListener('click', function() {
+    var original = document.getElementById("red-potion");
+    original.classList.remove('template');
+    var copy = original.cloneNode(true);
+    original.classList.add('template');
+    
+    copy.addEventListener('dragstart', () => {
+        copy.classList.add('dragging')
+    })
+
+    copy.addEventListener('dragend', () => {
+        copy.classList.remove('dragging')
+    })
+
+    controls.appendChild(copy);
+    
 })
 
 containers.forEach(container => {
     container.addEventListener('dragover', () => {
+        if(container.hasChildNodes()){
+            return;
+        }
         const draggable = document.querySelector('.dragging')
-        container.appendChild(draggable)
+        container.appendChild(draggable);
     })
 })
 
